c59732a5ff7274025154b1c310cf424981c8e7f8
Alexander Zelenyak <zzz.sochi@gmail.com>
2011-11-29 11:47:26 +0400

    Change version to 0.2.3 for pypi.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
         name = 'Flask-Gravatar',
-        version = '0.2.2',
+        version = '0.2.3',
         license='BSD',
         description = 'Small extension for Flask to make using Gravatar easy',
         long_description = open('README.rst').read(),
