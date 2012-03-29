77743112a207d0aed54c186fa8da6be8fa4fa14b
Honza Javorek <honza@javorek.net>
2012-03-29 16:42:18 +0300

    Processing of force_lower parameter was missing in constructor.

--- flaskext/gravatar.py
+++ flaskext/gravatar.py
@@ -31,6 +31,7 @@ class Gravatar(object):
         self.rating = rating
         self.default = default
         self.force_default = force_default
+        self.force_lower = force_lower
         self.use_ssl = use_ssl
 
         app.jinja_env.filters.setdefault('gravatar', self)
