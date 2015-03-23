e778ce674e3a7ee73f7e6eb45b72cbfd2f03b0d9
Jiri Kuncar <jiri.kuncar@cern.ch>
2015-03-24 00:36:32 +0100

    version: post-release version bump
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- flask_gravatar/version.py
+++ flask_gravatar/version.py
@@ -16,4 +16,4 @@ This file is imported by ``flask_gravatar.__init__``, and parsed by
 # Do not change the format of this next line. Doing so risks breaking
 # setup.py and docs/conf.py
 
-__version__ = "0.4.2"
+__version__ = "0.4.3.dev20150324"
